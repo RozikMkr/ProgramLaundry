e574d6839ff19e9747fd6cdc58db019bf8bddb56
Muhammad Khoirrur Rizqi <101788251+RozikMkr@users.noreply.github.com>
2023-01-06 02:18:10 +0700

    Update and rename jenis.java to Jenis.java

---
+++ Jenis.java
@@ -0,0 +1,9 @@
+public interface Jenis {
+    public void member();
+    public void baju();
+    public void sepatu();
+    public void karpet();
+    public void jaket();
+
+}
+
--- jenis.java
+++
@@ -1,7 +0,0 @@
-package rojik;
-
-public interface jenis {
-    public void jaket();
-    public void baju();
-    public void karpet();
-}
